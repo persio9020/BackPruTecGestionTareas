135a84b1610e99074ca58e39c3afbcd1457a958e
Hector Eduardo Leon Cruz <persio9020@gmail.com>
2025-03-08 02:38:23 -0500

    Ajustes autenticación

--- src/main/java/com/informa/tareas/config/AuthenticationManager.java
+++ src/main/java/com/informa/tareas/config/AuthenticationManager.java
@@ -1,9 +1,11 @@
 package com.informa.tareas.config;
 
 import io.jsonwebtoken.Claims;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,31 +15,40 @@ import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class AuthenticationManager implements ReactiveAuthenticationManager {
 
     private final JWTUtil jwtUtil;
 
     @Autowired
-    public  AuthenticationManager(JWTUtil jwtUtil) {
+    public AuthenticationManager(JWTUtil jwtUtil) {
         this.jwtUtil = jwtUtil;
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public Mono<Authentication> authenticate(Authentication authentication) {
         String authToken = authentication.getCredentials().toString();
-        String username = jwtUtil.getUsernameFromToken(authToken);
-        return Mono.just(jwtUtil.validateToken(authToken))
-            .filter(valid -> valid)
-            .switchIfEmpty(Mono.empty())
-            .map(valid -> {
-                Claims claims = jwtUtil.getAllClaimsFromToken(authToken);
-                List<String> rolesMap = claims.get("role", List.class);
-                return new UsernamePasswordAuthenticationToken(
+
+        return Mono.defer(() -> {
+            String username = jwtUtil.getUsernameFromToken(authToken);
+
+            if (!jwtUtil.validateToken(authToken)) {
+                return Mono.error(new RuntimeException("Token inválido o expirado"));
+            }
+
+            Claims claims = jwtUtil.getAllClaimsFromToken(authToken);
+            List<String> rolesMap = claims.get("role", List.class);
+
+            Authentication auth = new UsernamePasswordAuthenticationToken(
                     username,
                     null,
                     rolesMap.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList())
-                );
-            });
+            );
+
+            return Mono.just(auth);
+        }).onErrorResume(e -> {
+            log.error("Error de autenticacion", e);
+            return Mono.empty();
+        });
     }
 }
